3c81d3d88433dfd857c9e7c0ec54615e04160e53
marius.morar <marius.morar02@gmail.com>
2021-03-21 21:38:31 +0200

    Add suport for AddAutoMapper and Mediator

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,3 +1,6 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -38,6 +41,8 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
