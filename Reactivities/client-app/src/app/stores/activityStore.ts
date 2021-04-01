bbdaf2f6cf4ad71f2a74e5fcd69a3d3be7a69c70
marius.morar <marius.morar02@gmail.com>
2021-04-01 22:35:37 +0300

    Add activityStore

---
+++ Reactivities/client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,33 @@
+import { Activity } from './../models/activity';
+import { makeAutoObservable } from "mobx";
+import agent from '../api/agent';
+
+export default class ActivityStore {
+
+    activities:Activity[] = [];
+    selectedActivity: Activity | null= null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    loadActivities = async () => {
+        this.loadingInitial = true;
+
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach((activity) => {
+                activity.date = activity.date.split("T")[0];
+                this.activities.push(activity);
+              });
+              this.loadingInitial = false;
+        } catch (error) {
+            this.loadingInitial = false;
+            console.log(error);
+        }
+    }
+
+}
