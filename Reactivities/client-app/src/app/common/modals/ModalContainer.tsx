3d15e7ae06040ad429034f7ddfa2350ee828fe58
marius.morar <marius.morar02@gmail.com>
2021-04-18 19:42:03 +0300

    Add ModalContainer

---
+++ Reactivities/client-app/src/app/common/modals/ModalContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import { Modal } from "semantic-ui-react";
+
+export default observer(function ModalContainer() {
+  const { modalStore } = useStore();
+
+  return (
+    <Modal open={modalStore.modal.open} onClose={modalStore.closeModal}>
+      <Modal.Content>{modalStore.modal.body}</Modal.Content>
+    </Modal>
+  );
+});
