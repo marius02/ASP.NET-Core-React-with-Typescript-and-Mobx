9fea80c58e9c996f9cf040905b2e024af61079f8
marius.morar <marius.morar02@gmail.com>
2021-04-18 19:42:15 +0300

    Add RegisterForm

---
+++ Reactivities/client-app/src/features/users/RegisterForm.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Formik, Form, ErrorMessage } from "formik";
+import { Button, Header, Label } from "semantic-ui-react";
+import MyTextInput from "../../app/common/form/MyTextInput";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import * as Yup from "yup";
+import ValidationErrors from "../errors/ValidationsErrors";
+
+export default observer(function RegisterForm() {
+  const { userStore } = useStore();
+
+  return (
+    <Formik
+      initialValues={{
+        displayName: "",
+        username: "",
+        email: "",
+        password: "",
+        error: null,
+      }}
+      onSubmit={(values, { setErrors }) =>
+        userStore.register(values).catch((error) => setErrors({ error }))
+      }
+      validationSchema={Yup.object({
+        displayName: Yup.string().required(),
+        username: Yup.string().required(),
+        email: Yup.string().required().email(),
+        password: Yup.string().required(),
+      })}
+    >
+      {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
+        <Form
+          className="ui form error"
+          onSubmit={handleSubmit}
+          autoComplete="off"
+        >
+          <Header
+            as="h2"
+            content="Signup to Reactivities"
+            color="teal"
+            textAlign="center"
+          />
+          <MyTextInput
+            name="displayName"
+            placeholder="Display Name"
+          ></MyTextInput>
+          <MyTextInput name="username" placeholder="Username"></MyTextInput>
+          <MyTextInput name="email" placeholder="Email"></MyTextInput>
+          <MyTextInput
+            name="password"
+            placeholder="Password"
+            type="password"
+          ></MyTextInput>
+          <ErrorMessage
+            name="error"
+            render={() => <ValidationErrors errors={errors.error} />}
+          />
+          <Button
+            disabled={!isValid || !dirty || isSubmitting}
+            loading={isSubmitting}
+            positive
+            content="Register"
+            type="submit"
+            fluid
+          />
+        </Form>
+      )}
+    </Formik>
+  );
+});
